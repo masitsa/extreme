bf455d5e74d11148132f7c514308b6112f3bc363
Alvaro <amasitsa@live.com>
2015-11-12 10:07:22 +0300

    tinymce text editor added

---
+++ application/modules/administration/views/textarea.php
@@ -0,0 +1,11 @@
+
+    <h1>TinyMCE Getting Started Guide</h1>
+    <form method="post">
+        <textarea id="mytextarea" rows="10" class="form-control"></textarea>
+    </form>
+<script type="text/javascript" src="<?php echo base_url();?>assets/themes/tinymce/tinymce.min.js"></script>
+    <script type="text/javascript">
+        tinymce.init({
+            selector: "#mytextarea"
+        });
+    </script>
