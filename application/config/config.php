d8be8fbcd87a1de48092b67e36514fa54f986d73
PurityMonje <puritymwalozi@gmail.com>
2016-05-24 17:17:12 +0300

    uploaded

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/extreme/';
+$config['base_url'] = 'http://localhost/xtreme/';
 
 /*
 |--------------------------------------------------------------------------
